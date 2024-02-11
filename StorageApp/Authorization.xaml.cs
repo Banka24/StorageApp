28e508e3f476ef56aaeb48f49a27bbc9ea13b66e
Banka24 <Aq567489012@gmail.com>
2024-02-11 14:53:26 +0300

    refact5

--- StorageApp/Authorization.xaml.cs
+++ StorageApp/Authorization.xaml.cs
@@ -12,7 +12,10 @@ namespace StorageApp;
 /// </summary>
 public partial class Authorization
 {
-    private const string ErrorConnectionMessage = "Произошла ошибка, проверьте настройки подключения к сети и проверьте логи";
+    private const string SuccessMessage = "Авторизация прошла успешна.";
+
+    private const string ErrorConnectionMessage =
+        "Произошла ошибка, проверьте настройки подключения к сети и проверьте логи";
 
     private const string ErrorUserMessage = "Такого пользователя нет. Проверьте логин и пароль.";
 
@@ -44,8 +47,7 @@ public partial class Authorization
 
         try
         {
-            var user = await context.Workers.Include(i => i.Name)
-                .SingleOrDefaultAsync(i => i.Login == login && i.Password == password);
+            var user = await context.Workers.Include(i => i.Name).SingleOrDefaultAsync(i => i.Login == login && i.Password == password);
             return user;
         }
         catch (Exception ex)
@@ -57,27 +59,25 @@ public partial class Authorization
         return null;
     }
 
-    private async Task CheckUserAsync()
+    private async Task<bool> CheckUserAsync(string login, string password)
     {
-        var user = await GetUserAsync(LoginTextBox.Text, MyPassword.Password);
+        var user = await GetUserAsync(login, password);
 
         if (user is not null)
         {
             SharedContext.Name = user.Name.FirstName;
             SharedContext.Role = user.RankId;
-        }
-        else if (GetZeroUser(LoginTextBox.Text, MyPassword.Password) is not null)
-        {
-            SharedContext.Role = 1;
-            SharedContext.Name = "Админ";
-        }
-        else
-        {
-            MessageBox.Show(ErrorUserMessage);
-            return;
+            return true;
         }
 
-        ChangeWindow();
+        if (GetZeroUser(login, password) is not null)
+        {
+            SharedContext.Role = 1;//id role admin
+            SharedContext.Name = "Админ";//default username Admin
+            return true;
+        }
+
+        return false;
     }
 
     private void ChangeWindow()
@@ -89,7 +89,19 @@ public partial class Authorization
 
     private async void EnterButton_Click(object sender, RoutedEventArgs e)
     {
-        await Task.Run(CheckUserAsync);
+        var login = string.IsNullOrWhiteSpace(LoginTextBox.Text) ? null : LoginTextBox.Text;
+        var password = string.IsNullOrWhiteSpace(MyPassword.Password) ? null : MyPassword.Password;
+
+        if (login is null && password is null) return;
+
+        if (!await Task.Run(async () => await CheckUserAsync(login, password)))
+        {
+            MessageBox.Show(ErrorUserMessage);
+            return;
+        }
+
+        MessageBox.Show(SuccessMessage);
+        ChangeWindow();
     }
 
     private void ExitButton_Click(object sender, RoutedEventArgs e)
